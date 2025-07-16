d2c0cc1aeff597a596d8a568c41c61b0340cda1e
kimdonghuyn <say3356@naver.com>
2025-07-17 00:08:39 +0900

    feat: 회원가입 인터페이스 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Dto.java
@@ -0,0 +1,39 @@
+package com.loopers.interfaces.api.user;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.validation.constraints.NotNull;
+
+public class UserV1Dto {
+
+    public record SignUpRequest(
+            @NotNull
+            String name,
+            @NotNull(message = "성별은 필수입니다.")
+            GenderRequest gender,
+            @NotNull
+            String email,
+            @NotNull
+            String birth
+    ) {
+        enum GenderRequest {
+            M,
+            F
+        }
+    }
+
+    public record UserResponse(
+            Long userId,
+            String name,
+            GenderResponse gender,
+            String email,
+            String birth
+    ) {
+
+    }
+
+    enum GenderResponse {
+        M,
+        F
+    }
+}
