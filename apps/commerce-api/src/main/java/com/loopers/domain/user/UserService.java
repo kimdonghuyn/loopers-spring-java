8ed57c0372c49cecc07a779e4eaf205c145df4dc
kimdonghuyn <say3356@naver.com>
2025-07-18 11:17:58 +0900

    feat: 내 정보 조회 기능 구현

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -1,6 +1,7 @@
 package com.loopers.domain.user;
 
 import com.loopers.infrastructure.user.UserJpaRepository;
+import com.loopers.interfaces.api.user.UserV1Dto;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,21 @@ public class UserService {
         }
 
         userJpaRepository.save(userEntity);
+    }
+
+    public UserV1Dto.UserResponse getUserInfo(String userId) {
+        UserV1Dto.UserResponse response = new UserV1Dto.UserResponse(
+                "loopers123",
+                "hyun",
+                UserV1Dto.GenderResponse.F,
+                "loopers@naver.com",
+                "2002-10-10"
+        );
+
+        if (response.userId().equals(userId)) {
+            return response;
+        } else {
+            throw new CoreException(ErrorType.NOT_FOUND, "해당 유저를 찾을 수 없습니다.");
+        }
     }
 }
