2ceb7740e048559e9dbd8fa78cb6e95af6694b31
kimdonghuyn <say3356@naver.com>
2025-08-24 12:47:19 +0900

    feat: resilience4j 의존성 주성

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -21,11 +21,13 @@ dependencies {
     // DataFaker
     implementation("net.datafaker:datafaker:2.4.4")
 
-    //resilience4j
-    implementation("io.github.resilience4j:resilience5j-circuitbreaker")
-    implementation("io.github.resilience4j:resilience4j-java")
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-    // feign
+    // feign client
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    // resilience4j
+    implementation("io.github.resilience4j:resilience4j-spring-boot3")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 }
